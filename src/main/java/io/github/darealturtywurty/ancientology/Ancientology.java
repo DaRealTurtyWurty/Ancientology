5e81133e1093f80bd518c9dd4b72e3d8c139f682
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2022-01-22 19:02:14 +0000

    Add GeckoLib

--- src/main/java/io/github/darealturtywurty/ancientology/Ancientology.java
+++ src/main/java/io/github/darealturtywurty/ancientology/Ancientology.java
@@ -1,24 +1,34 @@
 package io.github.darealturtywurty.ancientology;
 
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.ItemStack;
-
 import io.github.darealturtywurty.ancientology.core.init.BlockEntityInit;
 import io.github.darealturtywurty.ancientology.core.init.BlockInit;
 import io.github.darealturtywurty.ancientology.core.init.EntityInit;
 import io.github.darealturtywurty.ancientology.core.init.FluidInit;
 import io.github.darealturtywurty.ancientology.core.init.ItemInit;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import software.bernie.geckolib3.GeckoLib;
 
 @Mod(Ancientology.MODID)
 public class Ancientology {
 
     public static final String MODID = "ancientology";
 
+    public static final CreativeModeTab ANCIENTOLOGY_ITEM_TAB = new CreativeModeTab(MODID) {
+        @Override
+        public ItemStack makeIcon() {
+            // TODO: add an icon
+            return ItemStack.EMPTY;
+        }
+    };
+
     public Ancientology() {
         final IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        
+        GeckoLib.initialize();
 
         BlockInit.BLOCKS.register(bus);
         ItemInit.ITEMS.register(bus);
@@ -26,14 +36,4 @@ public class Ancientology {
         BlockEntityInit.BLOCK_ENTITIES.register(bus);
         EntityInit.ENTITIES.register(bus);
     }
-
-    public static final CreativeModeTab ANCIENTOLOGY_ITEM_TAB = new CreativeModeTab(CreativeModeTab.getGroupCountSafe(),
-            MODID) {
-
-        @Override
-        public ItemStack makeIcon() {
-            // TODO add an icon
-            return ItemStack.EMPTY;
-        }
-    };
 }
