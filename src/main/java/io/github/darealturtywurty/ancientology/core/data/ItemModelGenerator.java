a35fe14f1a73ee664268b0da337c7a60da0b9f8f
NightBane <jzimmerman4444@gmail.com>
2022-01-25 15:49:41 -0700

    Bone and Bronze Armor

--- src/main/java/io/github/darealturtywurty/ancientology/core/data/ItemModelGenerator.java
+++ src/main/java/io/github/darealturtywurty/ancientology/core/data/ItemModelGenerator.java
@@ -19,19 +19,51 @@ public class ItemModelGenerator extends ItemModelProvider {
 
     @Override
     protected void registerModels() {
+    	// Resource Blocks
         defaultBlock(BlockInit.DEEPSLATE_TIN_ORE);
         defaultBlock(BlockInit.TIN_BLOCK);
         defaultBlock(BlockInit.BRONZE_BLOCK);
+        
+        // Resource Items
         defaultItem(ItemInit.RAW_TIN);
         defaultItem(ItemInit.TIN_INGOT);
         defaultItem(ItemInit.TIN_NUGGET);
+        defaultItem(ItemInit.COPPER_NUGGET);
         defaultItem(ItemInit.BRONZE_INGOT);
         defaultItem(ItemInit.BRONZE_NUGGET);
+        
+        // Flint Tools
         handheldItem(ItemInit.FLINT_AXE);
         handheldItem(ItemInit.FLINT_DAGGER);
         handheldItem(ItemInit.FLINT_HOE);
         handheldItem(ItemInit.FLINT_PICKAXE);
         handheldItem(ItemInit.FLINT_SHOVEL);
+        
+        // Bone Tools
+        handheldItem(ItemInit.BONE_AXE);
+        handheldItem(ItemInit.BONE_DAGGER);
+        handheldItem(ItemInit.BONE_HOE);
+        handheldItem(ItemInit.BONE_PICKAXE);
+        handheldItem(ItemInit.BONE_SHOVEL);
+        
+        // Bronze Tools
+        handheldItem(ItemInit.BRONZE_AXE);
+        handheldItem(ItemInit.BRONZE_SWORD);
+        handheldItem(ItemInit.BRONZE_HOE);
+        handheldItem(ItemInit.BRONZE_PICKAXE);
+        handheldItem(ItemInit.BRONZE_SHOVEL);
+        
+        // Bronze Armor
+        defaultItem(ItemInit.BRONZE_HELMET);
+        defaultItem(ItemInit.BRONZE_CHESTPLATE);
+        defaultItem(ItemInit.BRONZE_LEGGINGS);
+        defaultItem(ItemInit.BRONZE_BOOTS);
+        
+        // Bone Armor
+        dyeableItem(ItemInit.BONE_HELMET);
+        dyeableItem(ItemInit.BONE_CHESTPLATE);
+        dyeableItem(ItemInit.BONE_LEGGINGS);
+        dyeableItem(ItemInit.BONE_BOOTS);
         
     }
 
@@ -43,6 +75,12 @@ public class ItemModelGenerator extends ItemModelProvider {
     private void defaultItem(ItemRegistryObject<?> reg) {
         withExistingParent(reg.getId().getPath(), "item/generated").texture("layer0",
                 new ResourceLocation(reg.getId().getNamespace(), "item/" + reg.getId().getPath()));
+    }
+    
+    private void dyeableItem(ItemRegistryObject<?> reg) {
+        withExistingParent(reg.getId().getPath(), "item/generated").texture("layer0",
+                new ResourceLocation(reg.getId().getNamespace(), "item/" + reg.getId().getPath()))
+                .texture("layer1", new ResourceLocation(reg.getId().getNamespace(), "item/" + reg.getId().getPath() + "_overlay"));
     }
     
     private void handheldItem(ItemRegistryObject<?> reg) {
--- src/main/java/io/github/darealturtywurty/ancientology/core/materials/ToolMaterials.java
+++ src/main/java/io/github/darealturtywurty/ancientology/core/materials/ToolMaterials.java
@@ -1,73 +1,60 @@
 package io.github.darealturtywurty.ancientology.core.materials;
 
+import java.util.function.Supplier;
 import io.github.darealturtywurty.ancientology.core.init.ItemInit;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.crafting.Ingredient;
 
-public enum ToolMaterials implements Tier 
-{
-	//Tool Materials
-	flint(0, 59, 2.0F, 0.0F, 0, null),
-	bone(0, 59, 2.0F, 0.0F, 0, null),
-	bronze(1, 131, 4.0F, 1.0F, 5, ItemInit.BRONZE_INGOT.get()),
-	iron(2, 250, 6.0F, 2.0F, 14, Items.IRON_INGOT ),
-	iron_golden(2, 250, 12.0F, 2.0F, 22, Items.GOLD_INGOT),
-	iron_diamond(3, 1561, 8.0F, 3.0F, 10, Items.DIAMOND),	
-	iron_netherite(4, 2031, 9.0F, 4.0F, 15, Items.NETHERITE_INGOT)
-	;
-	
-	//Variables
-	private float speed, damage;
-	private int level, uses, enchantmentValue;
-	private Item repairMaterial;
-	
-	private ToolMaterials(int level, int uses, float speed, float damage, int enchantmentValue, Item repairMaterial)
-	{
-		this.level = level;
-		this.uses = uses;
-		this.speed = speed;
-		this.damage = damage;
-		this.enchantmentValue = enchantmentValue;
-		this.repairMaterial = repairMaterial;
-	}
+public enum ToolMaterials implements Tier {
+   FLINT(0, 59, 2.0F, 0.0F, 15, () -> {
+	   return Ingredient.of(Items.FLINT);
+   }),
+   BONE(0, 59, 2.0F, 0.0F, 15, () -> {
+	   return Ingredient.of(Items.BONE);
+	   }),
+   BRONZE(1, 131, 4.0F, 1.0F, 5, () -> {
+      return Ingredient.of(ItemInit.BRONZE_INGOT);
+   });
 
+   private final int level;
+   private final int uses;
+   private final float speed;
+   private final float damage;
+   private final int enchantmentValue;
+   private final Supplier<Ingredient> repairIngredient;
 
-	public Ingredient getRepairMaterial() {
-		return Ingredient.of(this.repairMaterial);// fromItems(this.repairMaterial);
-	}
+   private ToolMaterials(int level, int uses, float speed, float damage, int enchantmentValue, Supplier<Ingredient> repairIngredient) {
+      this.level = level;
+      this.uses = uses;
+      this.speed = speed;
+      this.damage = damage;
+      this.enchantmentValue = enchantmentValue;
+      this.repairIngredient = repairIngredient;
+   }
 
-	@Override
-	public int getUses() {
-		return this.uses;
-	}
+   public int getUses() {
+      return this.uses;
+   }
 
-	@Override
-	public float getSpeed() {
-		return this.speed;
-	}
+   public float getSpeed() {
+      return this.speed;
+   }
 
-	@Override
-	public float getAttackDamageBonus() {
-		return this.damage;
-	}
+   public float getAttackDamageBonus() {
+      return this.damage;
+   }
 
-	@Override
-	public int getLevel() {
-		return this.level;
-	}
+   public int getLevel() {
+      return this.level;
+   }
 
-	@Override
-	public int getEnchantmentValue() {
-		return this.enchantmentValue;
-	}
+   public int getEnchantmentValue() {
+      return this.enchantmentValue;
+   }
 
-	@Override
-	public Ingredient getRepairIngredient() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+   public Ingredient getRepairIngredient() {
+      return this.repairIngredient.get();
+   }
 
 }
-
